d690138b9455b7442f49cad81df47694f1d304e7
fjhgf <fjhgf@naver.com>
2025-01-13 07:55:47 +0900

    Add ddl.sql, oracle in gradle, securityConfig code for check board page

---
+++ ddl.sql
@@ -0,0 +1,5 @@
+show user;
+
+// 계정생성
+create user c##education identified by java1234;
+grant connect, resource, dba to c##education;
--- education-system/src/main/java/com/test/system/controller/MemberController.java
+++ education-system/src/main/java/com/test/system/controller/MemberController.java
@@ -1,5 +1,17 @@
 package com.test.system.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
 public class MemberController {
+	
+	@GetMapping("/board")
+	public String board() {
+		return "page/board";
+	}
+	
 
 }
