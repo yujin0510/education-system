1814ad639cf1912c76e61e3b16c68903381e3eb7
yujin0510 <yuyujinkim65@gmail.com>
2025-01-17 22:57:28 +0900

    Design: login 버튼 추가signup 페이지 작업

--- education-system/src/main/java/com/test/system/controller/MemberController.java
+++ education-system/src/main/java/com/test/system/controller/MemberController.java
@@ -3,6 +3,7 @@ package com.test.system.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import lombok.RequiredArgsConstructor;
 
